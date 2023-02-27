cf788a78204bdc0357f3399f01f727e1b5e5aea9
Kori2303 <125811456+Kori2303@users.noreply.github.com>
2023-02-27 13:58:09 +0300

    Add files via upload

---
+++ lol.py
@@ -0,0 +1,31 @@
+a = int(input("введите число: "))
+b = int(input("Введите число: "))
+c = int(input("Введите число: "))
+d = print(b^2-4*a*c)
+if d>0:
+    x1=int(input((-b+d**0.5)/a*2))
+    x2 = int(input((-b - d ** 0.5) / a * 2))
+print(x1,x2)
+elif d==0:
+x = int(input(-(b/a*2)))
+print(x)
+else d<0:
+print("корня нет")
+
+
+
+
+
+
+
+
+
+
+print(a*x^2+b*x+c)
+
+
+
+
+
+
+
