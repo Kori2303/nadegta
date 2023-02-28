cbf2a3a628c5bf8f9d20ebeafb1271f110859eeb
Kori2303 <125811456+Kori2303@users.noreply.github.com>
2023-02-28 22:53:47 +0300

    Add files via upload

---
+++ на 5.py
@@ -0,0 +1,13 @@
+a = int
+def parabol ():
+    a = int(input("Введите переменную a: "))
+while a == 0 :
+    print("a не должно быть равно 0")
+a=int(input("Введите переменную a:"))
+b=int(input("Введите перемнную B: "))
+x=int(input("Введите перемнную x: "))
+c=int(input("Введите перемнную c: "))
+y = (a*x)*2+b*x+c
+print ("Значение параболы = ", y)
+parabol()
+
