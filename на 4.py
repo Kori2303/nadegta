8515e41c01374f4b59f34400d97f7fcd26c06968
Kori2303 <125811456+Kori2303@users.noreply.github.com>
2023-03-01 00:21:24 +0300

    Update на 4.py

--- на 4.py
+++ на 4.py
@@ -1,23 +1,14 @@
-a = int(input("ведите число: "))
-b = int(input("ведите число: "))
-c = int(input("ведите число: "))
-d = b**2-4*a*c
-dis = d**0.5
-print (dis)
-if d<0:
-    print("корня нет")
-elif d == 0:
-    x = -b / (a * 2)
-    print (f"число {x}= x ")
-    y = a * x ** 2 + b * x + c
-    print(f"число {y}= y ")
-elif d>0:
-    x1 = - b + dis / a * 2
-    print(f"число {x1}= x1 ")
-    x2 = - b - dis / a * 2
-    print(f"число {x2}= x2 ")
-    y1 = a * x1 ** 2 + b * x1 + c
-    print(f"число {y1}= y1 ")
-    y2 = a * x2 ** 2 + b * x2 + c
-    print(f"число {y2}= y2 ")
+x_min = int(input("Min. x:"))
+#вводим минимальный х
+x_max = int(input("Max. x:"))
+#вводим максимальный х
+a = int(input("a:")) #вводим а
+b = int(input("a:")) #вводим b
+c = int(input("a:")) #вводим с
+for i in range (x_min, x_max + 1):
+    y = a*(i) **2 + b*(i) + c
+    #I кол-во повторений
+    print("Coordinates x =", i, ",y = ", y)
+    #координаты х, у
+
 
