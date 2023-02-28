66310ab648a607c3c1d4d5cf8df078005e7393ce
Kori2303 <125811456+Kori2303@users.noreply.github.com>
2023-02-28 21:47:01 +0300

    Add files via upload

--- lol.py
+++ lol.py
@@ -1,30 +1,18 @@
 a = int(input("введите число: "))
 b = int(input("Введите число: "))
 c = int(input("Введите число: "))
-d = print(b^2-4*a*c)
-if d>0:
-    x1=int(input((-b+d**0.5)/a*2))
-    x2 = int(input((-b - d ** 0.5) / a * 2))
-print(x1,x2)
-elif d==0:
-x = int(input(-(b/a*2)))
-print(x)
-else d<0:
-print("корня нет")
-
-
-
-
-
-
-
-
-
-
-print(a*x^2+b*x+c)
-
-
-
+d = b**2-4*a*c
+dis = d**0.5
+print (dis)
+if d<0:
+    print("корня нет")
+elif d == 0:
+    x = -b / (a * 2)
+    print(f"число {x}= корень")
+elif d>0:
+    x1 = - b + dis / a * 2
+    x2 = - b - dis / a * 2
+    print(f"число {x1} = 1 корень" , f"число {x2}= 2 корень ")
 
 
 
